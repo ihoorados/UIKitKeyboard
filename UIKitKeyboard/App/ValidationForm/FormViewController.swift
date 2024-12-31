cbb34a8fdb8f2f7b73eba38f225fe61cdbc33a42
Hoorad <hooradramezani@gmail.com>
2024-12-31 20:02:03 +0330

    add form view controller

---
+++ UIKitKeyboard/App/ValidationForm/FormViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FormViewController.swift
+//  UIKitKeyboard
+//
+//  Created by Hoorad on 12/31/24.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
